d7576657a37c2b52bb29c011dc68a5d14ea80237
Aiysosis <aiysosis@qq.com>
2022-11-19 16:31:17 +0800

    feat unmount

--- src/runtime-core/index.ts
+++ src/runtime-core/index.ts
@@ -6,6 +6,7 @@ export interface VNode {
 		[key: string]: string;
 	};
 	children: string | VNode[];
+	el: RendererElement;
 }
 
 //为什么采用这么宽泛的类型？ 因为面向全平台，
@@ -45,40 +46,65 @@ export function createRenderer(options: RendererOptions): Renderer {
 		createText: hostCreateText,
 	} = options;
 
-	function mountElement(vnode: VNode, container: RendererElement) {
-		//* -----------------------------------------以下为内部逻辑抽离-----------------------------------------
-		const patchProps = (el: RendererElement, key: string) => {
-			/*
-			 * props 可能有多种情况
-			 * 普通的键值对 id="app"
-			 * 只有键，不关心值 <buttn disabled/>
-			 * 只读 <input form="form1"/>
-			 * class 有可能传入的是对象 同时class有多种设置的方法，要选择性能最好的 el.className
-			 * //todo vue的绑定属性
-			 */
-			const type = typeof el[key];
-			const value = vnode.props[key];
+	//* -----------------------------------------以下为内部逻辑抽离-----------------------------------------
+	const patchProps = (
+		el: RendererElement,
+		key: string,
+		prevValue: any,
+		nextValue: any
+	) => {
+		/*
+		 * props 可能有多种情况
+		 * 普通的键值对 id="app"
+		 * 只有键，不关心值 <buttn disabled/>
+		 * 只读 <input form="form1"/>
+		 * class 有可能传入的是对象 同时class有多种设置的方法，要选择性能最好的 el.className
+		 * //todo vue的绑定属性
+		 */
+		const type = typeof el[key];
 
-			if (shouldSetAsProps(el, key, value)) {
-				//要设置的属性是 DOM Properties
-				//? 例如 buttn 按钮的 disable
-				if (type === "boolean" && value === "") {
-					el[key] = true;
-				} else {
-					el[key] = value;
-				}
+		if (key === "class") {
+			//如果 nextValue 是undefined，可能是清除样式的操作
+			//className 最快
+			el.className = nextValue || "";
+		} else if (shouldSetAsProps(el, key, nextValue)) {
+			//要设置的属性是 DOM Properties
+			//? 例如 buttn 按钮的 disable
+			if (type === "boolean" && nextValue === "") {
+				el[key] = true;
 			} else {
-				//设置的属性没有 DOM Properties, 使用 setAttribute
-				el.setAttribute(key, vnode[key]);
+				el[key] = nextValue;
 			}
-		};
+		} else {
+			//设置的属性没有 DOM Properties, 使用 setAttribute
+			el.setAttribute(key, nextValue);
+		}
+	};
 
-		//* -------------------------------------------以下为主流程--------------------------------------------
+	const shouldSetAsProps = (el: RendererElement, key: string, value: any) => {
+		//特殊处理
+		if (key === "form" && el.tagName === "input") return false;
+		//兜底
+		return key in el;
+	};
+
+	const unmount = (vnode: VNode) => {
+		//todo 调用生命周期函数 or 钩子函数
+		const el = vnode.el;
+		const parent = el.parentNode;
+		if (parent) parent.removeChild(el);
+	};
+
+	//* -------------------------------------------以下为主流程--------------------------------------------
+
+	function mountElement(vnode: VNode, container: RendererElement) {
 		const el = hostCreateElement(vnode.type);
+		//? 引用实际的 dom 元素，用于正确卸载
+		vnode.el = el;
 
 		if (vnode.props) {
 			for (const key in vnode.props) {
-				patchProps(el, key);
+				patchProps(el, key, null, vnode.props[key]);
 			}
 		}
 
@@ -107,30 +133,24 @@ export function createRenderer(options: RendererOptions): Renderer {
 		}
 	};
 
+	//todo render func
 	const render: RenderFn = (vnode, container) => {
 		//三种情况：渲染元素，更新元素
 		if (vnode) {
-			//渲染 or 更新，走patch
+			//* 渲染 or 更新，走patch
 			patch(container._vnode || null, vnode, container);
 		} else {
-			//如果没有vnode，说明此时是一个清空节点的操作
-			container.innerHTML = "";
+			//* 卸载
+			unmount(container._vnode);
 		}
 
-		//无论哪种情况，都要更新 container 的 _vnode 属性
+		//* 无论哪种情况，都要更新 container 的 _vnode 属性
 		container._vnode = vnode;
 	};
 
 	return {
 		render,
 	};
-}
-
-function shouldSetAsProps(el: RendererElement, key: string, value: string) {
-	//特殊处理
-	if (key === "form" && el.tagName === "input") return false;
-	//兜底
-	return key in el;
 }
 
 //* 抽离出的平台无关的节点操作逻辑
