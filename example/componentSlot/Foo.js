eab5c0412105f1962c6d2319eb6bcedd195e1805
Aiysosis <aiysosis@qq.com>
2022-11-26 14:49:53 +0800

    fix slots

--- example/componentSlot/Foo.js
+++ example/componentSlot/Foo.js
@@ -9,9 +9,9 @@ export const Foo = {
 
 		//* 在这里指定渲染的顺序（必须和 app.js中给定的属性名保持一致）
 		return h("div", {}, [
-			...renderSlots(this.$slots, "header"),
+			renderSlots(this.$slots, "header"),
 			foo,
-			...renderSlots(this.$slots, "footer"),
+			renderSlots(this.$slots, "footer"),
 		]);
 	},
 };
--- src/runtime-core/helpers/renderSlots.ts
+++ src/runtime-core/helpers/renderSlots.ts
@@ -1,6 +1,9 @@
 import { Slots } from "../componentSlots";
+import { createVNode } from "../vnode";
 
 export function renderSlots(slots: Slots, key: string) {
 	const slot = slots[key];
-	if (slot) return slot;
+	if (slot) {
+		return createVNode("div", {}, slot);
+	}
 }
