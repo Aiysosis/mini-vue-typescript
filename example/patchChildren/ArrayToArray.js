dbbbc90e3d2becfb152e5427c1424a3f569507b1
Aiysosis <aiysosis@qq.com>
2022-12-01 18:59:36 +0800

    feat diff part3 move element

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -43,19 +43,43 @@ import { h, ref } from "../../lib/aiyso-vue.esm.js";
 //+ 3. 复合情形（有元素删除，有元素新增，也有元素更换位置）
 //+ a b *c -d f g
 //+ a b +e *c f g
+// const prevChildren = [
+// 	h("p", { key: "A" }, "A"),
+// 	h("p", { key: "B" }, "B"),
+// 	h("p", { key: "C", id: "prev-c" }, "C"),
+// 	h("p", { key: "D" }, "D"),
+// 	h("p", { key: "F" }, "F"),
+// 	h("p", { key: "G" }, "G"),
+// ];
+// const nextChildren = [
+// 	h("p", { key: "A" }, "A"),
+// 	h("p", { key: "B" }, "B"),
+// 	h("p", { key: "E" }, "E"),
+// 	h("p", { key: "C", id: "next-c" }, "C"),
+// 	h("p", { key: "F" }, "F"),
+// 	h("p", { key: "G" }, "G"),
+// ];
+
+//+ 4. 纯移动的情形，应用 最长递增子序列算法
 const prevChildren = [
 	h("p", { key: "A" }, "A"),
 	h("p", { key: "B" }, "B"),
-	h("p", { key: "C", id: "prev-c" }, "C"),
+
+	h("p", { key: "C" }, "C"),
 	h("p", { key: "D" }, "D"),
+	h("p", { key: "E" }, "E"),
+
 	h("p", { key: "F" }, "F"),
 	h("p", { key: "G" }, "G"),
 ];
 const nextChildren = [
 	h("p", { key: "A" }, "A"),
 	h("p", { key: "B" }, "B"),
+
+	h("p", { key: "D" }, "D"),
 	h("p", { key: "E" }, "E"),
-	h("p", { key: "C", id: "next-c" }, "C"),
+	h("p", { key: "C" }, "C"),
+
 	h("p", { key: "F" }, "F"),
 	h("p", { key: "G" }, "G"),
 ];
